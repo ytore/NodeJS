5cc26d5e56fe64297340959a1a66980d4b92b80e
ytore <turekeshov+github@gmail.com>
2016-06-30 01:08:37 +0600

    Finished a small project on NodeJS.

---
+++ router.js
@@ -0,0 +1,15 @@
+
+function route(handle, pathname, response, request){
+  console.log("About to route a request for " + pathname);
+
+  if (typeof handle[pathname] === 'function'){
+    return handle[pathname](response, request);
+  } else {
+    console.log("No request handler was found for " + pathname);
+    response.writeHead(200, {"Content-Type" : "text/plain"});
+    response.write("404 Not found");
+    response.end();
+  }
+}
+
+exports.route = route;
